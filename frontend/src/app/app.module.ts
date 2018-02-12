9fa16d4da0dd8949f674c14bf16c835bd4080a4f
Michael Scales <mscales@mindwireless.com>
2018-02-12 08:36:22 -0600

    Add app-routing, home-routing, nav and home page.
    Stub home page with basic routes configured.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { HomeModule } from './home/home.module';
+import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
