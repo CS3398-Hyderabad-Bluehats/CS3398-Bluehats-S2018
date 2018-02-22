afc892f731e3fff191182e54405cea337a7fdb2e
Michael Scales <16503997+michaelscales88@users.noreply.github.com>
2018-02-21 18:16:20 -0600

    Development frontend scales (#50)
    * add basic home page and bootstrap
    
    * update default images and fix styling
    
    * add jumbotron and header

--- frontend/src/app/login/login.module.ts
+++ frontend/src/app/login/login.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { FormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
