3db8441310d8ec600dcf4cdcfcebe6b755b2f67e
Michael Scales <mscales@mindwireless.com>
2018-04-01 14:09:24 -0500

    added sidebar information

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,9 +7,13 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private show = false;
   public constructor(private titleService: Title ) { }
  
   public setTitle(newTitle: string) {
     this.titleService.setTitle( newTitle );
   }
+
+  toggleCollapse() {this.show = !this.show}
+
 }
