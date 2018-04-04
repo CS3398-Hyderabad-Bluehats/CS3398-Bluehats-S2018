c6184e6c53b58e5418ab759b81fa5e46a593445d
Michael Scales <mscales@mindwireless.com>
2018-04-03 20:40:35 -0500

    updated dependencies for sidebar

--- frontend/src/app/sidebar/sidebar.module.ts
+++ frontend/src/app/sidebar/sidebar.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SidebarComponent } from './sidebar.component';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   exports: [SidebarComponent],
   declarations: [SidebarComponent]
 })
