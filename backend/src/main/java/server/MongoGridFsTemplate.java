470a688f8686a5b0a9f95e589297a8490b3014a3
univeralh <jamestylergarrison@gmail.com>
2018-04-16 16:36:07 -0500

    MongoDB Added for Images

---
+++ backend/src/main/java/server/MongoGridFsTemplate.java
@@ -0,0 +1,37 @@
+package server;
+
+package com.javasampleapproach.mongodb.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+ 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+ 
+@Configuration
+public class MongoGridFsTemplate  extends AbstractMongoConfiguration{
+	
+	@Value("${jsa.mongo.address}")
+	private String mongoAddress; 
+	
+	@Value("${jsa.mongo.database}")
+	private String mongoDatabase;
+	
+	@Bean
+	public GridFsTemplate gridFsTemplate() throws Exception {
+	    return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
+	}
+	
+	@Override
+	protected String getDatabaseName() {
+		return mongoDatabase;
+	}
+ 
+	@Override
+	public Mongo mongo() throws Exception {
+		return new MongoClient(mongoAddress);
+	}
+}
