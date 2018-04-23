98cb2b3c6af88976cb32ef757abdf1ae5561a2a8
Michael Scales <mscales@mindwireless.com>
2018-04-23 15:14:00 -0500

    cleaning up for development merge

--- backend/src/test/java/server/Login/LoginControllerTest.java
+++ backend/src/test/java/server/Login/LoginControllerTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
